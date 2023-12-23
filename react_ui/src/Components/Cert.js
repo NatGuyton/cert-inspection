2fd1257e5bca83dd898de9976742fba505227038
Nat Guyton <nat@guyton.net>
2023-12-23 12:02:13 -0600

    font style refinements

--- react_ui/src/Components/Cert.js
+++ react_ui/src/Components/Cert.js
@@ -1,17 +1,17 @@
 function Cert(props) {
     let expiredtext = ""
     if (props.data.expired === true) {
-        expiredtext = <em style={{color: 'red'}}>EXPIRED</em>;
+        expiredtext = <em className="red">EXPIRED</em>;
     }
 
     let trustedtext = ""
     if (props.data.trusted === true) {
-        trustedtext = <> - <em style={{color: 'green'}}>IN TRUST STORE</em></>;
+        trustedtext = <> - <em className="green">IN TRUST STORE</em></>;
     }
 
     let sentbyserver = ""
     if (props.data.fromServer === true) {
-        sentbyserver = <> - <em style={{color: 'blue'}}>SENT BY SERVER</em></>;
+        sentbyserver = <> - <em className="blue">SENT BY SERVER</em></>;
     }
 
     // colorize validation string appropriately
@@ -22,9 +22,11 @@ function Cert(props) {
         let dashindex = validation.indexOf(" - ", 0);
         if (dashindex !== undefined) {
             if (validation.includes(" - 0:", 0)) {
-                validation = <>{validation.slice(0, dashindex)}<em style={{color: 'green'}}>{validation.slice(dashindex)}</em></>;
+                // validation = <>{validation.slice(0, dashindex)}<em style={{color: 'green'}}>{validation.slice(dashindex)}</em></>;
+                validation = <>{validation.slice(0, dashindex)}<em className="green">{validation.slice(dashindex)}</em></>;
             } else {
-                validation = <>{validation.slice(0, dashindex)}<em style={{color: 'red'}}>{validation.slice(dashindex)}</em></>;
+                // validation = <>{validation.slice(0, dashindex)}<em style={{color: 'red'}}>{validation.slice(dashindex)}</em></>;
+                validation = <>{validation.slice(0, dashindex)}<em className="red">{validation.slice(dashindex)}</em></>;
             }
         }
     }
@@ -37,21 +39,21 @@ function Cert(props) {
                 </pre>
             </div>
             <div className="cert-details">
-                Depth: {props.order} {trustedtext} {sentbyserver}
-                {validation !== "" && <><br/>Validation: {validation}</>}
-                <br/>Subject: {props.data.subject} 
-                <br/>Issuer: {props.data.issuer}
-                {props.data.hasOwnProperty('subjectAltName') && <><br/>SAN: {props.data.subjectAltName}</>}
-                <br/>notBefore: {props.data.notBefore}
-                <br/>notAfter: {props.data.notAfter} {expiredtext}
-                {props.data.hasOwnProperty('keyUsage') && <><br/>keyUsage: {props.data.keyUsage}</>}
-                {props.data.hasOwnProperty('extendedKeyUsage') && <><br/>extendedKeyUsage: {props.data.extendedKeyUsage}</>}
-                {props.data.hasOwnProperty('basicConstraints') && <><br/>basicConstraints: {props.data.basicConstraints}</>}
-                {props.data.hasOwnProperty('signature_algorithm') && <><br/>signatureAlgorithm: {props.data.signature_algorithm}</>}
-                {props.data.hasOwnProperty('serialnumber') && <><br/>serialNumber: {props.data.serialnumber}</>}
-                <br/>Fingerprint: {props.data['SHA-1 fingerprint']}
-                {props.data.hasOwnProperty('subjectKeyIdentifier') && <><br/>subjectKeyIdentifier: {props.data.subjectKeyIdentifier}</>}
-                {props.data.hasOwnProperty('authorityKeyIdentifier') && <><br/>authorityKeyIdentifier: {props.data.authorityKeyIdentifier}</>}
+                Depth: <span className="value">{props.order}</span> {trustedtext} {sentbyserver}
+                {validation !== "" && <><br/>Validation: <span className="value">{validation}</span></>}
+                <br/>Subject: <span className="value">{props.data.subject}</span>
+                <br/>Issuer: <span className="value">{props.data.issuer}</span>
+                {props.data.hasOwnProperty('subjectAltName') && <><br/>SAN: <span className="value">{props.data.subjectAltName}</span></>}
+                <br/>notBefore: <span className="value">{props.data.notBefore}</span>
+                <br/>notAfter: <span className="value">{props.data.notAfter} {expiredtext}</span>
+                {props.data.hasOwnProperty('keyUsage') && <><br/>keyUsage: <span className="value">{props.data.keyUsage}</span></>}
+                {props.data.hasOwnProperty('extendedKeyUsage') && <><br/>extendedKeyUsage: <span className="value">{props.data.extendedKeyUsage}</span></>}
+                {props.data.hasOwnProperty('basicConstraints') && <><br/>basicConstraints: <span className="value">{props.data.basicConstraints}</span></>}
+                {props.data.hasOwnProperty('signature_algorithm') && <><br/>signatureAlgorithm: <span className="value">{props.data.signature_algorithm}</span></>}
+                {props.data.hasOwnProperty('serialnumber') && <><br/>serialNumber: <span className="value">{props.data.serialnumber}</span></>}
+                <br/>Fingerprint: <span className="value">{props.data['SHA-1 fingerprint']}</span>
+                {props.data.hasOwnProperty('subjectKeyIdentifier') && <><br/>subjectKeyIdentifier: <span className="value">{props.data.subjectKeyIdentifier}</span></>}
+                {props.data.hasOwnProperty('authorityKeyIdentifier') && <><br/>authorityKeyIdentifier: <span className="value">{props.data.authorityKeyIdentifier}</span></>}
             </div>
         </div>
     );
